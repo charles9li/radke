55b11b36944a4b36c99dd27f7399fab04c1c4c4c
Charles Li <charles9li@berkeley.edu>
2019-02-12 11:14:01 -0800

    modified the equations so that sigma_d = f(psi_d)

--- class_Scales.py
+++ class_Scales.py
@@ -1,13 +1,13 @@
 import numpy as np
 import warnings
-from scipy.constants import e, k
+from scipy.constants import e, k, R, epsilon_0
 from scipy.optimize import root
 
 class Solution_Scales:
 
     solver_sigma_complete = False
 
-    def __init__(self, c, pKM, pH=5.8, pKa=5.6, C1=10, C2=2, Ns=2e18, T=293):
+    def __init__(self, c, pKM, pH=5.8, pKa=5.6, C1=10, C2=2, Ns=2e18, T=293, eps=79):
         self.c = c
         self.cH = 10**-pH
         self.cCl = self.c + self.cH
@@ -17,6 +17,7 @@ class Solution_Scales:
         self.C2 = C2
         self.Ns = Ns
         self.T = T
+        self.eps = eps
 
     def solver_sigma(self):
 
@@ -24,13 +25,25 @@ class Solution_Scales:
 
             psi_d, SM, SH = X0
 
-            # Equations
-            S = self.Ns - SM - SH
-            sigma_0 = -e*(SM + S)
-            sigma_beta = e*SM
-            sigma_d = e*S
+            # Calculate sigma_d
+            c_bulk_sum = 1000*np.sum([self.cCl, self.c, self.cH])
+            c_d_sum = 1000*((self.c + self.cH)*np.exp(-e*psi_d/(k*self.T)) + self.cCl*np.exp(e*psi_d/(k*self.T)))
+            sigma_d = -psi_d/abs(psi_d)*np.sqrt(2*R*self.T*self.eps*epsilon_0*(c_d_sum - c_bulk_sum))
+
+            # Compute quantities
             psi_beta = psi_d - sigma_d/self.C2
+            S = self.Ns - SM - SH
+            sigma_beta = e*SM
+            sigma_0 = -e*(S + SM)
             psi_0 = psi_beta + sigma_0/self.C1
+
+            # # Equations
+            # S = self.Ns - SM - SH
+            # sigma_0 = -e*(SM + S)
+            # sigma_beta = e*SM
+            # sigma_d = e*S
+            # psi_beta = psi_d - sigma_d/self.C2
+            # psi_0 = psi_beta + sigma_0/self.C1
 
             # Return objective or create solution attributes
             if get_values:
@@ -45,8 +58,8 @@ class Solution_Scales:
                 self.psi_d = psi_d
             else:
                 sigma_objective = (sigma_0 + sigma_beta + sigma_d)/sigma_0
-                KM_objective = self.KM - SM*self.cH/(SH*self.c)*np.exp(e*(psi_beta-psi_0)/(k*self.T))
-                Ka_objective = self.Ka - S*self.cH/SH*np.exp(-e*psi_0/(k*self.T))
+                KM_objective = (self.KM - SM*self.cH/(SH*self.c)*np.exp(e*(psi_beta-psi_0)/(k*self.T)))/self.KM
+                Ka_objective = (self.Ka - S*self.cH/SH*np.exp(-e*psi_0/(k*self.T)))/self.Ka
                 return np.array([sigma_objective, KM_objective, Ka_objective])
 
         # Set overflow to trigger warning
@@ -64,11 +77,10 @@ class Solution_Scales:
             return 10**np.mean([np.log10(K1), np.log10(K2)])
 
         # Initialize guess and starting c and KM values
-        X0 = np.array([-0.01, 0.2*self.Ns, 0.2*self.Ns])
+        X0 = np.array([-0.005973597351999846, 1.8921500128751857e+18, 8.052532531933648e+16])
         c_prev = 0.1
-        KM_prev = 10**-3
+        KM_prev = 10**-2.9
         X0 = guess_create(equations, X0, c_prev, KM_prev)
-        print(X0)
         c_curr = np.mean([self.c, c_prev])
         KM_curr = log_mean(self.KM, KM_prev)
 
@@ -95,7 +107,7 @@ class Solution_Scales:
         self.solver_sigma_complete = True
 
 
-sol = Solution_Scales(.1, 2.9)
+sol = Solution_Scales(1e-3, 2.9)
 sol.solver_sigma()
 print(sol.psi_0)
 print(sol.psi_beta)
