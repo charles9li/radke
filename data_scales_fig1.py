8d35e2f7b00f2f6854f4406854bf1990799db75f
Charles Li <charles9li@berkeley.edu>
2019-02-12 18:03:54 -0800

    added data file for fig 1 of scales and marker+line file

---
+++ data_scales_fig1.py
@@ -0,0 +1,23 @@
+import numpy as np
+import csv
+
+# Salt list
+salts = ['LiCl', 'NaCl', 'KCl', 'CsCl']
+
+# Initiate dictionaries
+c_data = {}
+zeta_data = {}
+
+# Extract data from CSVs
+for salt in salts:
+    with open('data/scales_fig1_data_'+salt+'.csv') as csvfile:
+        datafile = csv.reader(csvfile, delimiter=',', quotechar='|')
+        c = []
+        zeta = []
+        for row in datafile:
+            c += [float(row[0])]
+            zeta += [float(row[1])]
+    c = np.array(c)
+    zeta = np.array(zeta)
+    c_data[salt] = c
+    zeta_data[salt] = zeta
