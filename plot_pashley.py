a23f3f569b202c2ddcf2d3875f82eada88bf6c20
Charles Li <charles9li@berkeley.edu>
2019-02-14 21:54:11 -0800

    plots 6e-2 M data from fig 1 of Pashley

--- plot_pashley.py
+++ plot_pashley.py
@@ -1,4 +1,18 @@
 import matplotlib.pyplot as plt
+from scipy.constants import e, k
 from data_pashley import *
+from class_Pashley import *
 
 # Figure 1
+# psi_d_list_fig1 = np.ones(len(pashley_fig1_data))
+# for data in pashley_fig1_data:
+#     c = data.c
+data = pashley_fig1_data[0]
+c = data.c
+D = data.D
+FR = data.FR
+plt.scatter(D, FR*1e-3)
+sol = PashleySolution(c, -0.05, np.linspace(D[0], D[-1])*1e-9)
+sol.compute_FR()
+plt.semilogy(sol.D_list*1e9, sol.FR_list*1e3)
+plt.show()
