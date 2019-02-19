d90a637ab706fe945f7bb4cbc8b68d775ca7235d
Charles Li <charles9li@berkeley.edu>
2019-02-19 13:22:45 -0800

    added data for fig 2-4

--- data_pashley.py
+++ data_pashley.py
@@ -11,7 +11,7 @@ class PashleyData:
 
 
 # Figure 1
-c_list = ['6e-2', '1e-2', '1e-3', '1e-4']
+c_list = ['1e-4', '1e-3', '1e-2', '6e-2']
 pashley_fig1_data = []
 for c in c_list:
     with open('data/pashley_fig1_data_' + c + '.csv') as csvfile:
@@ -24,3 +24,48 @@ for c in c_list:
     D = np.array(D)
     FR = np.array(FR)
     pashley_fig1_data += [PashleyData(float(c), D, FR)]
+
+# Figure 2
+c_list = ['4e-5', '1e-4', '1e-3', '1e-2']
+pashley_fig2_data = []
+for c in c_list:
+    with open('data/pashley_fig2_data_' + c + '.csv') as csvfile:
+        datafile = csv.reader(csvfile, delimiter=',', quotechar='|')
+        D = []
+        FR = []
+        for row in datafile:
+            D += [float(row[0])]
+            FR += [float(row[1])]
+    D = np.array(D)
+    FR = np.array(FR)
+    pashley_fig2_data += [PashleyData(float(c), D, FR)]
+
+# Figure 3
+c_list = ['4e-5', '3e-4', '1e-3']
+pashley_fig3_data = []
+for c in c_list:
+    with open('data/pashley_fig3_data_' + c + '.csv') as csvfile:
+        datafile = csv.reader(csvfile, delimiter=',', quotechar='|')
+        D = []
+        FR = []
+        for row in datafile:
+            D += [float(row[0])]
+            FR += [float(row[1])]
+    D = np.array(D)
+    FR = np.array(FR)
+    pashley_fig3_data += [PashleyData(float(c), D, FR)]
+
+# Figure 4
+c_list = ['4e-5', '1e-4', '1e-3']
+pashley_fig4_data = []
+for c in c_list:
+    with open('data/pashley_fig4_data_' + c + '.csv') as csvfile:
+        datafile = csv.reader(csvfile, delimiter=',', quotechar='|')
+        D = []
+        FR = []
+        for row in datafile:
+            D += [float(row[0])]
+            FR += [float(row[1])]
+    D = np.array(D)
+    FR = np.array(FR)
+    pashley_fig4_data += [PashleyData(float(c), D, FR)]
