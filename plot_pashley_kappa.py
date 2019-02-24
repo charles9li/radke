0c20812bc2bd2ebbf43275cb193ad129c4268a80
Charles Li <charles9li@berkeley.edu>
2019-02-24 15:44:58 -0800

    added data for all 4 figures

--- plot_pashley_kappa.py
+++ plot_pashley_kappa.py
@@ -10,23 +10,106 @@ def compute_kappa(c, T=298, epsilon=79):
     return np.sqrt(2*rho*e**2/(epsilon*epsilon_0*k*T))
 
 
+# Plot control
+plot_fig1 = False
+plot_fig2 = False
+plot_fig3 = False
+plot_fig4 = True
+
 # Figure 1
-plt.figure(1)
-pashley_fig1_c = []
-pashley_fig1_slope = []
-pashley_fig1_kappa = []
-start_index = [5, 0, 0, 5]
-i = 0
-for data in pashley_fig1_data:
-    pashley_fig1_c += [data.c]
-    pashley_fig1_kappa += [compute_kappa(data.c)]
-    slope, intercept, r_value, p_value, std_err = linregress(data.D[i:], np.log(data.FR[i:]))
-    pashley_fig1_slope += [slope]
-    i += 1
-    plt.scatter(data.D, data.FR)
-    plt.semilogy(data.D, np.exp(intercept)*np.exp(slope*data.D))
-pashley_fig1_c = np.array(pashley_fig1_c)
-pashley_fig1_slope = np.array(pashley_fig1_slope)
-pashley_fig1_kappa = np.array(pashley_fig1_kappa)
+if plot_fig1:
+    plt.figure(1)
+    pashley_fig1_c = []
+    pashley_fig1_slope = []
+    pashley_fig1_kappa = []
+    start_index = [5, 6, 1, 5]
+    i = 0
+    for data in pashley_fig1_data:
+        start = start_index[i]
+        pashley_fig1_c += [data.c]
+        pashley_fig1_kappa += [compute_kappa(data.c)]
+        slope, intercept, r_value, p_value, std_err = linregress(data.D[start:], np.log(data.FR[start:]))
+        pashley_fig1_slope += [slope]
+        i += 1
+        plt.scatter(data.D, data.FR, label='%.0e' % data.c)
+        plt.semilogy(data.D, np.exp(intercept)*np.exp(slope*data.D))
+    pashley_fig1_c = np.array(pashley_fig1_c)
+    pashley_fig1_slope = np.array(pashley_fig1_slope)
+    pashley_fig1_kappa = np.array(pashley_fig1_kappa)
+    plt.xlabel('D [nm]')
+    plt.ylabel('F/R [$\mu$N/m]')
+    plt.legend(title='Concentration [M]')
+
+# Figure 2
+if plot_fig2:
+    plt.figure(2)
+    pashley_fig2_c = []
+    pashley_fig2_slope = []
+    pashley_fig2_kappa = []
+    start_index = [0, 0, 0, 0]
+    i = 0
+    for data in pashley_fig2_data:
+        start = start_index[i]
+        pashley_fig2_c += [data.c]
+        pashley_fig2_kappa += [compute_kappa(data.c)]
+        slope, intercept, r_value, p_value, std_err = linregress(data.D[start:], np.log(data.FR[start:]))
+        pashley_fig2_slope += [slope]
+        i += 1
+        plt.scatter(data.D, data.FR, label='%.0e' % data.c)
+        plt.semilogy(data.D, np.exp(intercept)*np.exp(slope*data.D))
+    pashley_fig2_c = np.array(pashley_fig2_c)
+    pashley_fig2_slope = np.array(pashley_fig2_slope)
+    pashley_fig2_kappa = np.array(pashley_fig2_kappa)
+    plt.xlabel('D [nm]')
+    plt.ylabel('F/R [$\mu$N/m]')
+    plt.legend(title='Concentration [M]')
+
+# Figure 3
+if plot_fig3:
+    plt.figure(3)
+    pashley_fig3_c = []
+    pashley_fig3_slope = []
+    pashley_fig3_kappa = []
+    start_index = [4, 8, 8]
+    i = 0
+    for data in pashley_fig3_data:
+        start = start_index[i]
+        pashley_fig3_c += [data.c]
+        pashley_fig3_kappa += [compute_kappa(data.c)]
+        slope, intercept, r_value, p_value, std_err = linregress(data.D[start:], np.log(data.FR[start:]))
+        pashley_fig3_slope += [slope]
+        i += 1
+        plt.scatter(data.D, data.FR, label='%.0e' % data.c)
+        plt.semilogy(data.D, np.exp(intercept)*np.exp(slope*data.D))
+    pashley_fig3_c = np.array(pashley_fig3_c)
+    pashley_fig3_slope = np.array(pashley_fig3_slope)
+    pashley_fig3_kappa = np.array(pashley_fig3_kappa)
+    plt.xlabel('D [nm]')
+    plt.ylabel('F/R [$\mu$N/m]')
+    plt.legend(title='Concentration [M]')
+
+# Figure 4
+if plot_fig4:
+    plt.figure(4)
+    pashley_fig4_c = []
+    pashley_fig4_slope = []
+    pashley_fig4_kappa = []
+    start_index = [5, 6, 5]
+    i = 0
+    for data in pashley_fig4_data:
+        start = start_index[i]
+        pashley_fig4_c += [data.c]
+        pashley_fig4_kappa += [compute_kappa(data.c)]
+        slope, intercept, r_value, p_value, std_err = linregress(data.D[start:], np.log(data.FR[start:]))
+        pashley_fig4_slope += [slope]
+        i += 1
+        plt.scatter(data.D, data.FR, label='%.0e' % data.c)
+        plt.semilogy(data.D, np.exp(intercept)*np.exp(slope*data.D))
+    pashley_fig4_c = np.array(pashley_fig4_c)
+    pashley_fig4_slope = np.array(pashley_fig4_slope)
+    pashley_fig4_kappa = np.array(pashley_fig4_kappa)
+    plt.xlabel('D [nm]')
+    plt.ylabel('F/R [$\mu$N/m]')
+    plt.legend(title='Concentration [M]')
 
 plt.show()
