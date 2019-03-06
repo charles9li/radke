774978b09e070ddf77e80a02e39848ae53fb6f9e
Charles Li <charles9li@berkeley.edu>
2019-03-05 20:56:08 -0800

    initial commit

---
+++ radii.py
@@ -0,0 +1,36 @@
+"""
+IONIC RADII
+===========
+
+Provides crystollographic and Stoke's ionic radii for a number of ions.
+
+"""
+
+
+###############
+# IONIC RADII #
+###############
+
+R_Li = 60e-12	# Li+ radius	(m)
+R_Na = 98e-12	# Na+ radius	(m)
+R_K = 133e-12	# K+ radius		(m)
+R_Rb = 148e-12	# Rb+ radius	(m)
+R_Be = 31e-12	# Be^2+ radius	(m)
+R_Mg = 65e-12	# Mg^2+ radius	(m)
+R_Ca = 99e-12	# Ca^2+ radius	(m)
+R_Sr = 113e-12	# Sr^2+ radius	(m)
+R_F = 136e-12	# F- radius		(m)
+R_Cl = 181e-12	# Cl- radius	(m)
+R_Br = 185e-12	# Br- radius	(m)
+R_I = 216e-12	# I- radius		(m)
+
+
+#########################
+# IONIC RADII (HYDRATED) #
+#########################
+
+R_Li_hyd = 3.82e-10 # Li+ hydrated radius   (m)
+R_Na_hyd = 3.52e-10 # Na+ hydrated radius   (m)
+R_K_hyd = 3.31e-10  # K+ hydrated radius    (m)
+R_Rb_hyd = 3.29e-10 # Rb+ hydrated radius   (m)
+R_Cs_hyd = 3.29e-10 # Cs+ hydrated radius   (m)
